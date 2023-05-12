b6d7a1822d5d1fb20da236791865f8ec4ce1b9c1
Jin Hang <jinhang@chinacici.com>
2023-05-12 15:39:23 +0800

    feature(minigrep):重构minigrep

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -16,6 +16,19 @@ fn main() {
         .expect("Should have been able to read the file");
 
     // println!("With text:\n{contents}");
+
+    // ---snip---
+}
+
+// ---snip---
+
+impl Config{
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Config { query, file_path}
+    }
 }
 
 struct Config {
