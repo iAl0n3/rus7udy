7ca209b612f417ec9850218e63e01e557aea5d6e
Jin Hang <ngu2a@icloud.com>
2023-06-01 07:46:33 +0800

    feature(guess_game):新增比较大小

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,18 +1,26 @@
 use rand::Rng; //trait
+use std::cmp::Ordering;
 use std::io; //prelude
 
 fn main() {
     println!("Guessing Game");
     
     let secret_number = rand::thread_rng().gen_range(1, 101);
-    
     println!("神秘数字是：{}", secret_number);
-
     println!("Guess a number");
     
     let mut guess = String::new();
     
     io::stdin().read_line(&mut guess).expect("无法读取行");
     
+    // shadow
+    let guess: u32 = guess.trim().parse().expect("请输入数字");
+
     println!("你猜测的数是：{}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("太小了"),
+        Ordering::Greater => println!("太大了"),
+        Ordering::Equal => println!("恭喜你猜对了"),
+    }
 }
